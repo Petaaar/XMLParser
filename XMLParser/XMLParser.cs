7339639860ce53c3948f1416fe7342375f478efe
Petaaar <psihosocial@abv.bg>
2018-05-12 16:44:51 +0300

    Added abillity to read all items.

--- XMLParser/XMLParser.cs
+++ XMLParser/XMLParser.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 using System.Xml;
 
 namespace XMLParser
@@ -8,7 +9,6 @@ namespace XMLParser
     /// </summary>
     class XMLParser
     {
-
         public static void Main()
         {
             new XMLParser();
@@ -25,8 +25,6 @@ namespace XMLParser
 
             doc.Load(path);
 
-            //GetProps(doc);
-
             ParseXML(doc.GetElementsByTagName("nameSpace")[0]);
         }
 
@@ -34,7 +32,7 @@ namespace XMLParser
         /// Recursively parses a given <see cref="XmlNode"/>.
         /// </summary>
         /// <param name="root"><see cref="XmlNode"/> node to start from.</param>
-        public static void ParseXML(XmlNode root)
+        public void ParseXML(XmlNode root)
         {
             if (root is XmlElement)
             {
@@ -52,24 +50,53 @@ namespace XMLParser
         /// Searches a given <see cref="XmlNode"/> and checks it's attributes.
         /// </summary>
         /// <param name="node"><see cref="XmlNode"/> to search and parse.</param>
-        private static void Search(XmlNode node)
+        private void Search(XmlNode node)
         {
-            if (node.Name == "nameSpace")
+            if (node.Name == "nameSpace") //parsing the working namespace
                 if (node.Attributes["name"] != null)
                     if (node.Attributes["name"].Value == "SASH.Custom")
                         Console.WriteLine(node.Attributes["name"].Value);
 
             if (node.Name == "ref")
-                if (node.Attributes["using"] != null)
+                if (node.Attributes["using"] != null) //parsing all "using" directives
                     Console.WriteLine(node.Attributes["using"].Value);
 
-            if (node.Name == "class")
+            if (node.Name == "class") // parsing the class name/type/protection level
                 if (node.Attributes["protectionLevel"] != null && node.Attributes["type"] != null &&
                     node.Attributes["name"] != null)
-                    if (node.Attributes["protectionLevel"].Value != "" && node.Attributes["type"].Value != "" &&
-                        node.Attributes["name"].Value != "")
+                    if (node.Attributes["name"].Value != "")
                         Console.WriteLine($"{node.Attributes["protectionLevel"].Value} {node.Attributes["type"].Value} " +
                             $"{node.Attributes["name"].Value}");
+
+            if (node.Name == "item") //parsing PRIVATE fields
+                if (node.Attributes["type"] != null && node.Attributes["returnType"] != null && node.InnerText != null)
+                    if (node.InnerText != " ")
+                        Console.WriteLine($"private {node.Attributes["type"].Value} {node.Attributes["returnType"].Value} " + 
+                            $"{node.InnerText}");
+
+            if (node.Name == "encapsulate") //encapsulation property
+                if (node.InnerText != null)
+                    if (node.InnerText.ToLower() == "true" || node.InnerText.ToLower() == "false")
+                        Console.WriteLine(node.InnerText);
+                    else Console.WriteLine("NO ENCAPSULATION!");
+
+            if (node.Name == "publicItem")//parsing publicItems property
+                if (node.Attributes["type"] != null && node.Attributes["returnType"] != null && node.InnerText != null)
+                    if (node.Attributes["type"].Value != " " && node.Attributes["returnType"].Value != " " && node.InnerText != " ")
+                        Console.WriteLine($"public {node.Attributes["type"].Value} {node.Attributes["returnType"].Value}" +
+                            $" {node.InnerText}");
+
+            if (node.Name == "method")
+                if (node.Attributes["type"] != null && node.Attributes["returnType"] != null && node.InnerText != null)
+                    if (node.Attributes["type"].Value != " " && node.Attributes["returnType"].Value != " " && node.InnerText != " ")
+                        Console.WriteLine($"private {node.Attributes["type"].Value} {node.Attributes["returnType"].Value}" +
+                            $" {node.InnerText}");
+
+            if (node.Name == "publicMethod")
+                if (node.Attributes["type"] != null && node.Attributes["returnType"] != null && node.InnerText != null)
+                    if (node.Attributes["type"].Value != " " && node.Attributes["returnType"].Value != " " && node.InnerText != " ")
+                        Console.WriteLine($"private {node.Attributes["type"].Value} {node.Attributes["returnType"].Value}" +
+                            $" {node.InnerText}");
         }
     }
 }
