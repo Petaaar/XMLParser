b666987a8b2657c3a45144b2e07421bc076b1854
Petaaar <psihosocial@abv.bg>
2018-05-13 19:43:44 +0300

    Updated the parser.

---
+++ XMLParser/ClassParser.cs
@@ -0,0 +1,92 @@
+﻿namespace XMLParser
+{
+    /// <summary>
+    /// Parses the "class" property of the XML file.
+    /// </summary>
+    class ClassParser
+    {
+        #region Private
+
+        private string protectionLevel;
+        private string type;
+        private string className; 
+
+        #endregion Private
+
+        #region Public
+
+        public string ProtectionLevel
+        {
+            get
+            {
+                if (protectionLevel != null)
+                    return protectionLevel;
+                return string.Empty;
+            }
+            private set
+            {
+                if (value != null)
+                    protectionLevel = value;
+            }
+        }
+
+        public string Type
+        {
+            get
+            {
+                if (type != null)
+                    return type;
+                return string.Empty;
+            }
+
+            private set
+            {
+                if (value != null)
+                    type = value;
+            }
+        }
+
+        public string ClassName
+        {
+            get
+            {
+                if (className != null)
+                    return className;
+                return string.Empty;
+            }
+            private set
+            {
+                if (value != null && value != string.Empty)
+                    className = value;
+            }
+        }
+
+        public ClassParser() : this(null,null,null) { } //for no arguments given
+
+        public ClassParser(string className) : this(null,null,className) { } //for only one argument given
+
+        public ClassParser(string protectionLevel, string className) : this(protectionLevel, null, className) { }
+        
+        public ClassParser(string type, int placeholder, string className) : this(null,type,className) { }
+
+        public ClassParser(string protectionLevel, string type, string className)
+        {
+            this.protectionLevel = protectionLevel;
+            this.type = type;
+            this.className = className;
+        }
+
+        public string Parse()
+        { 
+            if (type == null && protectionLevel != null) //has only protection level
+                return $"{this.protectionLevel} {className}";
+            if (type == null && protectionLevel == null) //has only name
+                return $"{className}";
+            if (type != null && protectionLevel == null) //has only type
+                return $"{type} {className}";
+            return $"{protectionLevel} {type} {className}"; //have everything;
+        }
+
+        #endregion Public
+    }
+}
--- XMLParser/XMLParser.cs
+++ XMLParser/XMLParser.cs
@@ -9,10 +9,93 @@ namespace XMLParser
     /// </summary>
     class XMLParser
     {
-        public static void Main()
+        #region Private
+
+        private const string tab = "    ";
+
+        private string Namespace(string value) => $"namespace {value}" + "\n{";
+
+        private string Using(string collection) => $"{tab}using {collection};";
+
+        /// <summary>
+        /// Searches a given <see cref="XmlNode"/> and checks it's attributes.
+        /// </summary>
+        /// <param name="node"><see cref="XmlNode"/> to search and parse.</param>
+        private string Search(XmlNode node)
         {
-            new XMLParser();
+            #region Namespace
+            if (node.Name == "nameSpace") //parsing the working namespace
+                if (node.Attributes["name"] != null)
+                    if (node.Attributes["name"].Value == "SASH.Custom")
+                        return Namespace(node.Attributes["name"].Value);
+                    else return ("Invalid namespace!");
+            #endregion Namespace
+
+            #region Reference
+            if (node.Name == "ref")
+                if (node.Attributes["using"] != null) //parsing all "using" directives
+                    return Using(node.Attributes["using"].Value);
+            #endregion Reference
+
+            #region Class
+            if (node.Name == "class") // parsing the class name/type/protection level
+                if (node.Attributes["protectionLevel"] != null && node.Attributes["type"] != null && node.Attributes["name"] != null)
+                    return new ClassParser(node.Attributes["protectionLevel"].Value, node.Attributes["type"].Value,
+                            node.Attributes["name"].Value).Parse();
+                else if (node.Attributes["protectionLevel"] != null && node.Attributes["name"] != null)
+                    return new ClassParser(node.Attributes["protectionLevel"].Value, node.Attributes["name"].Value).Parse();
+                else if (node.Attributes["type"] != null && node.Attributes["name"] != null)
+                    return new ClassParser(node.Attributes["type"].Value, 0, node.Attributes["name"].Value).Parse();
+                else return new ClassParser(node.Attributes["name"].Value).Parse();
+            #endregion Class
+
+            #region Item
+            if (node.Name == "item") //parsing PRIVATE fields
+                if (node.Attributes["type"] != null && node.Attributes["returnType"] != null && node.InnerText != null)
+                    if (node.InnerText != " ")
+                        Console.WriteLine($"private {node.Attributes["type"].Value} {node.Attributes["returnType"].Value} " + 
+                            $"{node.InnerText}");
+            #endregion Item
+
+            #region Encapsulation
+            if (node.Name == "encapsulate") //encapsulation property
+                if (node.InnerText != null)
+                    if (node.InnerText.ToLower() == "true" || node.InnerText.ToLower() == "false")
+                        Console.WriteLine(node.InnerText);
+                    else Console.WriteLine("NO ENCAPSULATION!");
+            #endregion Encapsulation
+
+            #region PublicItem
+            if (node.Name == "publicItem")//parsing publicItems property
+                if (node.Attributes["type"] != null && node.Attributes["returnType"] != null && node.InnerText != null)
+                    if (node.Attributes["type"].Value != " " && node.Attributes["returnType"].Value != " " && node.InnerText != " ")
+                        Console.WriteLine($"public {node.Attributes["type"].Value} {node.Attributes["returnType"].Value}" +
+                            $" {node.InnerText}");
+            #endregion PublicItem
+
+            #region Method
+            if (node.Name == "method")
+                if (node.Attributes["type"] != null && node.Attributes["returnType"] != null && node.InnerText != null)
+                    if (node.Attributes["type"].Value != " " && node.Attributes["returnType"].Value != " " && node.InnerText != " ")
+                        Console.WriteLine($"private {node.Attributes["type"].Value} {node.Attributes["returnType"].Value}" +
+                            $" {node.InnerText}");
+            #endregion Method
+
+            #region PublicMethod
+            if (node.Name == "publicMethod")
+                if (node.Attributes["type"] != null && node.Attributes["returnType"] != null && node.InnerText != null)
+                    if (node.Attributes["type"].Value != " " && node.Attributes["returnType"].Value != " " && node.InnerText != " ")
+                        Console.WriteLine($"private {node.Attributes["type"].Value} {node.Attributes["returnType"].Value}" +
+                            $" {node.InnerText}");
+            #endregion PublicMethod
+
+            return "";
         }
+
+        #endregion
+
+        #region Public
+        public static void Main() => new XMLParser();
 
         public XMLParser() : this(@"c:\Users\petar\Desktop\Example.sashs")
         {
@@ -29,14 +112,16 @@ namespace XMLParser
         }
 
         /// <summary>
-        /// Recursively parses a given <see cref="XmlNode"/>.
+        /// Recursively parses <see cref="XmlDocument"/> by given <see cref="XmlNode"/> - <paramref name="root"/>.
         /// </summary>
         /// <param name="root"><see cref="XmlNode"/> node to start from.</param>
         public void ParseXML(XmlNode root)
         {
             if (root is XmlElement)
             {
-                Search(root);
+                var searchRes = Search(root);
+                if (searchRes != "")
+                    Console.WriteLine(searchRes);
             }
 
             if (root.HasChildNodes)
@@ -45,58 +130,7 @@ namespace XMLParser
                 ParseXML(root.NextSibling);
 
         }
-
-        /// <summary>
-        /// Searches a given <see cref="XmlNode"/> and checks it's attributes.
-        /// </summary>
-        /// <param name="node"><see cref="XmlNode"/> to search and parse.</param>
-        private void Search(XmlNode node)
-        {
-            if (node.Name == "nameSpace") //parsing the working namespace
-                if (node.Attributes["name"] != null)
-                    if (node.Attributes["name"].Value == "SASH.Custom")
-                        Console.WriteLine(node.Attributes["name"].Value);
-
-            if (node.Name == "ref")
-                if (node.Attributes["using"] != null) //parsing all "using" directives
-                    Console.WriteLine(node.Attributes["using"].Value);
-
-            if (node.Name == "class") // parsing the class name/type/protection level
-                if (node.Attributes["protectionLevel"] != null && node.Attributes["type"] != null &&
-                    node.Attributes["name"] != null)
-                    if (node.Attributes["name"].Value != "")
-                        Console.WriteLine($"{node.Attributes["protectionLevel"].Value} {node.Attributes["type"].Value} " +
-                            $"{node.Attributes["name"].Value}");
-
-            if (node.Name == "item") //parsing PRIVATE fields
-                if (node.Attributes["type"] != null && node.Attributes["returnType"] != null && node.InnerText != null)
-                    if (node.InnerText != " ")
-                        Console.WriteLine($"private {node.Attributes["type"].Value} {node.Attributes["returnType"].Value} " + 
-                            $"{node.InnerText}");
-
-            if (node.Name == "encapsulate") //encapsulation property
-                if (node.InnerText != null)
-                    if (node.InnerText.ToLower() == "true" || node.InnerText.ToLower() == "false")
-                        Console.WriteLine(node.InnerText);
-                    else Console.WriteLine("NO ENCAPSULATION!");
-
-            if (node.Name == "publicItem")//parsing publicItems property
-                if (node.Attributes["type"] != null && node.Attributes["returnType"] != null && node.InnerText != null)
-                    if (node.Attributes["type"].Value != " " && node.Attributes["returnType"].Value != " " && node.InnerText != " ")
-                        Console.WriteLine($"public {node.Attributes["type"].Value} {node.Attributes["returnType"].Value}" +
-                            $" {node.InnerText}");
-
-            if (node.Name == "method")
-                if (node.Attributes["type"] != null && node.Attributes["returnType"] != null && node.InnerText != null)
-                    if (node.Attributes["type"].Value != " " && node.Attributes["returnType"].Value != " " && node.InnerText != " ")
-                        Console.WriteLine($"private {node.Attributes["type"].Value} {node.Attributes["returnType"].Value}" +
-                            $" {node.InnerText}");
-
-            if (node.Name == "publicMethod")
-                if (node.Attributes["type"] != null && node.Attributes["returnType"] != null && node.InnerText != null)
-                    if (node.Attributes["type"].Value != " " && node.Attributes["returnType"].Value != " " && node.InnerText != " ")
-                        Console.WriteLine($"private {node.Attributes["type"].Value} {node.Attributes["returnType"].Value}" +
-                            $" {node.InnerText}");
-        }
+        #endregion
+        
     }
 }
