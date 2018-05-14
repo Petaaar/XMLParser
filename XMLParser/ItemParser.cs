bcb1dedf3d6acbd998e242eca55ba2e9741d1bab
Petaaar <psihosocial@abv.bg>
2018-05-14 16:38:16 +0300

    Added "ItemParser" class

---
+++ XMLParser/ItemParser.cs
@@ -0,0 +1,28 @@
+﻿namespace XMLParser
+{
+    /// <summary>
+    /// A class that parses every type of field. Cannot be inherited.
+    /// </summary>
+    sealed class ItemParser
+    {
+        private string type;
+
+        private string returnType;
+
+        private string name;
+
+        private readonly bool isPrivate;
+
+        public ItemParser() : this(null, null, null, true) { }
+
+        public ItemParser(string returnType, string name, bool isPrivate) : this(null, returnType, name, isPrivate) { }
+
+        public ItemParser(string type, string returnType, string name, bool isPrivate)
+        {
+            this.type = type;
+            this.returnType = returnType;
+            this.name = name;
+            this.isPrivate = isPrivate;
+        }
+    }
+}
