7c4e69788dce8a151f691015e2c7d505b6ad9942
Petaaar <psihosocial@abv.bg>
2018-05-15 14:42:25 +0300

    Added "MethodParser" class.

---
+++ XMLParser/MethodParser.cs
@@ -0,0 +1,10 @@
+﻿namespace XMLParser
+{
+    /// <summary>
+    /// Parses all methods, written in the XML(.sashs) file. Cannot be inherited.
+    /// </summary>
+    sealed class MethodParser
+    {
+
+    }
+}
