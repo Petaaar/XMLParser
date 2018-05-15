09b19be323c35f6e6c49b9a584f093abfd75f13f
Petaaar <psihosocial@abv.bg>
2018-05-15 14:54:46 +0300

    Added "IParser" interface.

---
+++ XMLParser/IParser.cs
@@ -0,0 +1,11 @@
+﻿/// <summary>
+/// Represents a XML node parser.
+/// </summary>
+interface IParser
+{
+    /// <summary>
+    /// Method to parse given type of node.
+    /// </summary>
+    /// <returns></returns>
+    string Parse();
+}
