ddb48e005ef68958da46e669a58bc415b0a67bfc
Petaaar <psihosocial@abv.bg>
2018-05-29 15:37:30 +0300

    Generic classes are now a thing in the program.

--- XMLParser/ClassParser.cs
+++ XMLParser/ClassParser.cs
@@ -10,6 +10,7 @@
         private string protectionLevel;
         private string type;
         private static string className;
+        private bool isGeneric;
 
         #endregion Private
 
@@ -69,23 +70,37 @@
         {
             this.protectionLevel = protectionLevel;
             this.type = type;
+
             if (className2 != null && className2 != string.Empty)
-            {
                 className = className2;
+
+            if (className2.EndsWith("{T}"))
+            {
+                className = className2.Remove(className2.Length - 3, 3);
+                isGeneric = true;
             }
-            if (className == null) System.Console.WriteLine("NULL");
-            if (className == string.Empty) System.Console.WriteLine("EMPTY");
+            if (className == null) System.Console.WriteLine("NO CLASS NAME!");
+            if (className == string.Empty) System.Console.WriteLine("EMPTY CLASS NAME!");
         }
 
         public string Parse()
         {
-            if (type == null && protectionLevel != null) //has only protection level
+            if (type == null && protectionLevel != null && !isGeneric) //has only protection level and isn't generic
                 return $"C{this.protectionLevel} {className}";
-            if (type == null && protectionLevel == null) //has only name
+            else if (type == null && protectionLevel != null && isGeneric) //has only protection level and IS generic
+                return $"C{this.protectionLevel} {className}<T>";
+
+            if (type == null && protectionLevel == null && !isGeneric) //has only name and isn't generic
                 return $"C{className}";
-            if (type != null && protectionLevel == null) //has only type
+            else if (type == null && protectionLevel == null && isGeneric) //has only name and IS generic
+                return $"C{className}<T>";
+            if (type != null && protectionLevel == null && !isGeneric) //has only type and isn't generic
                 return $"C{type} {className}";
-            return $"C{protectionLevel} {type} {className}"; //have everything;
+            else if (type != null && protectionLevel == null && isGeneric) //has only type and IS generic
+                return $"C{type} {className}<T>";
+            if (type != null && protectionLevel != null && className != null && isGeneric) //have everything and IS generic
+                return $"C{protectionLevel} {type} {className}<T>";
+            return $"C{protectionLevel} {type} {className}"; //have everything and isn't generic;
         }
 
         #endregion Public
